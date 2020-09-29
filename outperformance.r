4aa9f5b77d32cbb538d945836f64a732f9cfaacd
William Franz Lamberti <wlamber2@gmu.edu>
2020-09-29 10:50:32 -0400

    Add files via upload

---
+++ outperformance.r
@@ -0,0 +1,23 @@
+#outperformance measure
+
+#ALL-IDB2
+all_lamb<-0.9615
+all<-c(0.8972, 0.9384, 0.9523,
+      0.9567, 0.9611
+       )
+
+mean((all_lamb/all)-1)#0.02227561
+
+
+#C-NMC
+c_lamb<-0.909
+cnmc<-c(0.869, 0.867,
+        0.857, 0.879
+        )
+
+mean((c_lamb/cnmc)-1)#0.04731982
+
+#overall
+mean( ( c(c_lamb/cnmc, all_lamb/all ) -1 )  )#0.03340637
+
+#
