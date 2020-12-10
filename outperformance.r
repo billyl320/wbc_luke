f46dccab705001b27d83c44874eb822bca1f1d20
William Franz Lamberti <wlamber2@gmu.edu>
2020-12-10 16:51:04 -0500

    Update outperformance.r

--- outperformance.r
+++ outperformance.r
@@ -1,24 +1,24 @@
 #outperformance measure
 
 #ALL-IDB2
-all_lamb<-0.9615
+all_lamb<-1.00
 all<-c(0.8972, 0.9384, 0.9524,
       0.9567, 0.9611
        )
 
-mean((all_lamb/all)-1)#0.0222544
+mean((all_lamb/all)-1)#0.06318711
 
 
 #C-NMC
-c_lamb<-0.919
+c_lamb<-0.901
 cnmc<-c(0.869, 0.867,
         0.857, 0.879
         )
 
-mean((c_lamb/cnmc)-1)#0.04731982
+mean((c_lamb/cnmc)-1)#0.03810249
 
 
 #overall
-mean( ( c(c_lamb/cnmc, all_lamb/all ) -1 )  )#0.03851533
+mean( ( c(c_lamb/cnmc, all_lamb/all ) -1 )  )#0.05203839
 
 #
