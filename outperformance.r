22a18dc9ff1ca66a6b435da54612233a6085684b
William Franz Lamberti <wlamber2@gmu.edu>
2021-07-02 09:36:46 -0400

    Update outperformance.r

--- outperformance.r
+++ outperformance.r
@@ -4,10 +4,10 @@
 all_lamb<-1.00
 all<-c(0.8972, 0.9384, 0.9524,
       0.9567, 0.9611, 0.9920,
-       0.9950
+      0.9950
        )
 
-mean((all_lamb/all)-1)#0.06318711
+mean((all_lamb/all)-1)#0.0470036
 
 
 #C-NMC
@@ -20,6 +20,6 @@ mean((c_lamb/cnmc)-1)#0.03810249
 
 
 #overall
-mean( ( c(c_lamb/cnmc, all_lamb/all ) -1 )  )#0.05203839
+mean( ( c(c_lamb/cnmc, all_lamb/all ) -1 )  )#0.04376683
 
 #
